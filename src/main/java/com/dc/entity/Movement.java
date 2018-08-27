c194576e2e6872a2b8a1feefa4a7de1625cbc4b4
CASTRO Diogo <dicastro@europe.corp.altran.com>
2018-08-27 17:43:10 +0100

    added movement

--- src/main/java/com/dc/entity/Movement.java
+++ src/main/java/com/dc/entity/Movement.java
@@ -3,12 +3,15 @@ package com.dc.entity;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Movement {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public long id;
 	
 	public String username;
